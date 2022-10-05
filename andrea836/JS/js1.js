7ef7e2e2d5536ca9c992037bef343baa31dcdc68
andrea836 <andrea.garzavrgs@uanl.edu.mx>
2022-10-05 09:15:50 -0500

    js

--- andrea836/JS/js1.js
+++ andrea836/JS/js1.js
@@ -4,3 +4,12 @@ console.error("hola");
 console.log("El tipo 34: "+ typeof 34 + ".");
 console.log(`Tipo de 34: ${typeof 34}.`);
 console.log(`Ejemplo de operador logico ${ 5 > 2 || 3 < 1}`);
+console.log(10 < 2? "parte verdadera":"parte falsa");
+
+//declarador de variables//
+let temp=10;
+let mensaje;
+mensaje = temp < 10 ? "lleva chamarra": "no lleva chamara";
+console.log(mensaje);
+const SEMESTRE = 9;
+SEMESTRE =3;
