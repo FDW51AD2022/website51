b54abdef7f791e9c6192bde41750431e7510c130
andrea836 <andrea.garzavrgs@uanl.edu.mx>
2022-10-19 08:26:38 -0500

    suma js5

---
+++ andrea836/JS/js5.js
@@ -0,0 +1,8 @@
+function sumar() {
+    var x = parseInt(document.getElementById("valor1").value);
+    var y = parseInt(document.getElementById("valor2").value);
+    document.getElementById("resultado").innerHTML = x + y;
+
+
+    
+}
