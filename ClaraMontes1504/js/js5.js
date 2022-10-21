8d6a4bd4cbd38cb14bef383da18200630a62f891
Clara Itzzel Montes Sánchez <claraimontes15@gmail.com>
2022-10-21 15:07:08 -0500

    Se hace un ciclo if

--- ClaraMontes1504/js/js5.js
+++ ClaraMontes1504/js/js5.js
@@ -1,4 +1,32 @@
 /* CALCULADORA */
+function calcular() {
+    let num = parseInt(document.getElementById("num").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+    let operacion = document.getElementById("operacion").value;
+    let resul= document.getElementById("resul");
+
+    let total = 0;
+    
+    if (operacion == "+") {
+        total = num + num2;
+
+    } 
+    else if (operacion == "-"){
+        total = num - num2;
+    }
+    else if (operacion == "x"){
+        total = num * num2;
+    } 
+    else {
+        total = num / num2;
+    }
+
+    resul.value = total;
+}
+
+
+
+
 
 
 
