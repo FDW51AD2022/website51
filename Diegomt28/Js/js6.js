f7b19da3b39713be743a1b8d45bedaf8783f1fc3
Diegomt28 <diegomacias2802@gmail.com>
2022-10-26 22:14:44 -0500

    cambios

--- Diegomt28/Js/js6.js
+++ Diegomt28/Js/js6.js
@@ -1,24 +1,46 @@
-function Imagenes() {
-    let num  = parseInt(document.getElementById("num").value);
-    let imagen = document.getElementById('img');
+/* programa Calculadora */
 
-    switch (num) {
-        case 1:
-            imagen.src = "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_960_720.jpg";
-            break;
-        case 2:
-            imagen.src = "https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-1024x681.jpg";
-            break;
-        case 3:
-            imagen.src = "https://images4.alphacoders.com/936/936378.jpg";
-            break;
-        case 4:
-            imagen.src = "https://c4.wallpaperflare.com/wallpaper/39/346/426/digital-art-men-city-futuristic-night-hd-wallpaper-preview.jpg";
-            break;
-        case 5:
-            imagen.src = "https://wallpaper.dog/large/10883325.jpg";
-            break;
-        default:
-            break;
+function suma(){
+
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r=parseFloat(a)+parseFloat(b);
+    document.f1.c.value=r;
+    
     }
+    
+function resta(){
+
+var a = document.f1.a.value;
+var b = document.f1.b.value;
+var r = document.f1.c.value;
+r=parseFloat(a)-parseFloat(b);
+document.f1.c.value=r;
+
 }
+
+function multiplicacion(){
+
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r=parseFloat(a)*parseFloat(b);
+    document.f1.c.value=r;
+
+}
+
+function division(){
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r=parseFloat(a)/parseFloat(b);
+    document.f1.c.value=r;
+    alert(`El resultado de la division es: `+r)
+
+    }
+
+
+
+
+/* programa Imagenes */
