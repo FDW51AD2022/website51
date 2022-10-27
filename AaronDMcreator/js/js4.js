7794724084256f0ac20494d8d98aa67c9043c066
Aaron Abraham Diaz Medellin <aaron.diazmln@uanl.edu.mx>
2022-10-27 18:10:23 -0500

    Aplicando estilos al js8 ejercicio de la tabla de multiplicar

--- AaronDMcreator/js/js4.js
+++ AaronDMcreator/js/js4.js
@@ -1,53 +1,12 @@
-function divisores(numero) {
-    let total = 0;
+let HoraInicialToma = Number(prompt("Ingresa la hora de la toma inicial: "));
+let Frecuencia = Number(prompt("Indica la frecuencia de la toma: "))
 
-    for(let i=1; i<=numero; i++) {
-        if(numero % i == 0) {
-            total ++;
-        }
-    }
-    return total;
+
+cont = 0
+
+let toma= HoraInicialToma; 
+while(toma<=23){
+    cont = cont + 1
+    console.log("Toma ", cont,": ", toma);
+    toma = toma + Frecuencia;
 }
-
-// Funcion en forma de variable constante
-const divisoresV2 = function(numero) {
-    let total = 0;
-
-    for(let i=1; i<=numero; i++) {
-        if(numero % i == 0) {
-            total ++;
-        }
-    }
-    return total;
-};
-
-// Funciones Flecha, sirve para hacer las funciones mas compactas
-const divisoresV3 = numero => {
-    let total = 0;
-
-    for(let i=1; i<=numero; i++) {
-        if(numero % i == 0) {
-            total ++;
-        }
-    }
-    return total;
-};
-
-function calcular() {
-    let numero = parseInt(document.getElementById("num").value);
-    let resultado = document.getElementById("resul");
-    
-    resultado.value = divisores(numero);
-    
-    // Imprimir
-    console.log("Divisores: ",divisores(numero));
-}
-
-
-
-
-// Llamar Funcion
-// console.log("Divisores: ",divisores(33));
-// console.log("DivisoresV2: ",divisoresV2(33));
-// console.log("DivisoresV3: ",divisoresV3(33));
-// console.log("DivisoresV3: ",divisores(numero));
