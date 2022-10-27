9b228d18baf4c5f7bd68421ba4db086fd8dab4a6
Roble Treviño <roble.trevinozmrp@uanl.edu.mx>
2022-10-27 15:50:13 -0500

    Cambios

--- robletrevino/JS/js3.js
+++ robletrevino/JS/js3.js
@@ -1,12 +1,11 @@
 cantidadNumeros = Number(prompt("Ingresa la cantidad de números a mostrar:"))
 
-document.write("Actividad 1<br/>")
 
 document.write("Cantidad de números a mostrar: ", cantidadNumeros," (tipo de dato: ",typeof(cantidadNumeros),")")
 
 encontrado = 0;
 for(i = 1; encontrado <= cantidadNumeros-1; i++){
-    //  Se revisa si es Par o no par
+  
     if(i % 2 == 0){
 
         for((j = (i / 2));(j >= 1); j--){
@@ -20,4 +19,5 @@ for(i = 1; encontrado <= cantidadNumeros-1; i++){
             }
     }
     auxEntero = 0;
+    
 }
