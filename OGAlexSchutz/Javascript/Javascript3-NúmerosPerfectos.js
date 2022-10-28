91754d5e8c5d4d6ed28535fc44c21b9387b590e0
OGAlexSchutz <alejandro.lopezgnzlz@outlook.com>
2022-10-28 16:12:53 -0500

    Se realizaron cambios de estabilidad

--- OGAlexSchutz/Javascript/Javascript3-NúmerosPerfectos.js
+++ OGAlexSchutz/Javascript/Javascript3-NúmerosPerfectos.js
@@ -9,11 +9,11 @@ for(i = 1; encontrado <= cantidadNumeros-1; i++){
     if(i % 2 == 0){
         for((j = (i / 2));(j >= 1); j--){
             if(i % j == 0){
-                auxEntero = auxEntero + j;
+                EnAUX = EnAUX + j;
             }
         }
-        if(auxEntero == i){
-            document.write("<br/> Número perfecto: ",auxEntero);
+        if(EnAUX == i){
+            document.write("<br/> Número perfecto: ",EnAUX);
             encontrado++
             }
     }
