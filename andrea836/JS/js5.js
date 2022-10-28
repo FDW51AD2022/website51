97e6c2660c18adfcfd401da8c128b4fe24cbf18e
andrea836 <andrea.garzavrgs@uanl.edu.mx>
2022-10-28 16:55:24 -0500

    js 5 ejercicio imagenes

--- andrea836/JS/js5.js
+++ andrea836/JS/js5.js
@@ -18,3 +18,30 @@ function dividir() {
     var y = parseInt(document.getElementById("valor2").value);
     document.getElementById("resultado").innerHTML = x / y;    
 }
+/**************************************/
+
+function captura(){
+    let num= parseInt(document.getElementById("imagen1").value);
+    let captura = document.getElementById("img");
+    switch (num) {
+        case 1:
+            imagen.src =;
+            break;
+        case 2:
+            imagen.src =;
+            break;
+        case 3:
+            imagen.src =;
+            break;
+        case 4:
+            imagen.src =;
+            break;
+        case 5:
+            imagen.src =;
+            break;
+        default:
+            break;                
+
+    }
+
+}
