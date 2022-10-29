26abd94db4fa05edc44da2574d8d1c5e71b45072
valeriacortes2319 <valeriacm56@gmail.com>
2022-10-28 21:28:22 -0500

    modificaciones de js5 ultimo ej

--- valeriacortes2319/JS/js5.js
+++ valeriacortes2319/JS/js5.js
@@ -57,4 +57,26 @@ function verimagen(){
     }
 }
 
+/*Tercer Ejercicio*/
+function Num(){
+    var r=document.getElementById("numero").value;
+    document.write("<table border='2'>");
+    document.write("<tr>");
+    document.write("<td colspane='5'>tabla"+ r);
+    document.write("</tr>");
+    for (var i = 1; i <=10; i++) {
+        m=i*r;
+
+        document.write("<tr>");
+        document.write("<td>"+ r);
+        document.write("<td> x");
+        document.write("<td>"+ i);
+        document.write("<td> =");
+        document.write("<td>" + m);
+        document.write("</tr>");
+
+};
+ document.write("</table>");
+}
+
 /*5preguntas abieras y 3 programas javascript tipo, calculo, ciclos, if switch, variables, cajas normales*/
