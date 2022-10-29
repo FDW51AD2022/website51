8e968757943e65f44612fcb3378d36d9e3f126fd
andrea836 <andrea.garzavrgs@uanl.edu.mx>
2022-10-28 19:43:39 -0500

    js3 medicinas

--- andrea836/JS/js3.js
+++ andrea836/JS/js3.js
@@ -13,3 +13,15 @@ numeros.forEach(function(numero){
     suma+=numero;
 })
 console.log(parseInt(suma))
+/**********************************/
+
+let tomaini = Number(prompt("Hora de la primera toma"));
+let frecuencia = Number(prompt("Frecuencia de la toma"));
+contador = 0
+let hora = tomaini;
+while(hora<=23){
+    contador = contador + 1
+    console.log("Hora de toma ", contador,": ", hora,"hrs");
+    hora = hora + frecuencia;
+}
+
