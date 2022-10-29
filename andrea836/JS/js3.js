9ed32702dd5effc8eff777b8b2585ebcd49c65a9
andrea836 <andrea.garzavrgs@uanl.edu.mx>
2022-10-28 19:42:11 -0500

    js3

---
+++ andrea836/JS/js3.js
@@ -0,0 +1,15 @@
+let numero = prompt("Ingresa un numero");
+while(isNaN(numero) || numero ==0){
+    numero = prompt("ingresa un numero");
+}
+let numeros = [];
+for(i=0; i < numero; i++){
+    if (numero % i===0){
+        numeros.push(parseInt(i));
+    }
+}
+let suma=0;
+numeros.forEach(function(numero){
+    suma+=numero;
+})
+console.log(parseInt(suma))
