cebe52791ad7d1b6a885e19ad67cd11b2615217e
valeriacortes2319 <valeriacm56@gmail.com>
2022-10-28 20:50:14 -0500

    modificaciones de todas las actividades

--- valeriacortes2319/JS/js4.js
+++ valeriacortes2319/JS/js4.js
@@ -1,31 +1,49 @@
-let inicio =new Date();
-let medicamento = [`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,
-`10`,`11`,`12`,`13`,`14`,`15`,`16`,`17`,`18`,`19`,`20`,
-`21`,`22`,`23`,`24`]
-let horaio =inicio.getDate();
-console.log(`Horario` `${medicamento[horaio]}`);
+/*funciones*/ /*%=division*/ 
+function divisores(numero){
+    let total = 0;
+    for(let i=1; i<=numero;i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+}
 
-console.log();
-let horas1 = inicio.gethours();
-let horas2 = inicio.gethours();
-let horas3 = inicio.gethours();
-let horas4 = inicio.gethours();
+/*Definir una funcion anonima*/
+const divisioresV2 = function(numero){
+    let total = 0;
+    for(let i=1; i<=numero;i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+};
 
-let jornada = horas1 >= 0 ? `1` : `0`;
-let horas = horas2 >= 0 ? `5` : `0`;
-let jornadAs = horas1 >= 0 ? '2' : `0`;
-let horAs = horas2 >= 0 ? `11` : `0`;
-let jornadas1 = horas1 >= 0 ? `3` : `0`;
-let horA1 = horas2 >= 0 ? `17` : `0`;
-let jornadas = horas1 >= 0 ? `4` : `0`;
 
-console.log(`${horas % 23} ${jornada} : ${horas}`);
-console.log(`${horas % 23} ${jornadAs} : ${horas}`);
-console.log(`${horas % 23} ${jornadas1} : ${horas1}`);
-console.log(`${horas % 23} ${jornadas} : ${horaa}`);
+/*Declaracion de funciones flecha*/
+const divisioresV3 = numero =>{
+    let total = 0;
+    for(let i=1; i<=numero;i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+};
 
-let frecuenciadeinicio = new Date();
-let iniciodetoma =[`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,
-`10`,`11`,`12`];
-let tomar = frecuenciadeinicio.getDate();
-console.log(`frecuencia` `: ${iniciodetoma[hora]}`);
+/*console.log(perfectos(4));*/
+
+function calcular(){
+    let numero = parseInt(document.getElementById("num").value);
+
+    let resultado = document.getElementById("resul");
+
+    resultado.value = divisores(numero);
+}
+/*Llamar la función, antes estaba esto aqui pero se sustituyo con resultado.value.
+console.log("divisiores",divisores(numero));*/
+
+/*Llamar la función*/
+/*console.log("divisioresV2",divisioresV2(33));
+console.log("divisioresV3",divisioresV2(33));*/
--- valeriacortes2319/JS/js5.js
+++ valeriacortes2319/JS/js5.js
@@ -1,49 +1,63 @@
-/*funciones*/ /*%=division*/ 
-function divisores(numero){
+/*Primer ejercicio*/
+function operaciones_calculo(numero, numero2, operaciones){
     let total = 0;
-    for(let i=1; i<=numero;i++){
-        if(numero % i == 0){
-            total++;
-        }
+    if (operaciones == "suma") {
+        total=numero+numero2
+        return total;
     }
-    return total;
+    else if (operaciones == "resta"){
+        total=numero-numero2
+        return total;
+    }
+    else if (operaciones == "multiplicacion"){
+        total=numero*numero2
+        return total;
+    }
+    else if(operaciones == "division"){
+        total=numero/numero2
+        return total;
+    }
 }
-
-/*Definir una funcion anonima*/
-const divisioresV2 = function(numero){
-    let total = 0;
-    for(let i=1; i<=numero;i++){
-        if(numero % i == 0){
-            total++;
-        }
-    }
-    return total;
-};
-
-
-/*Declaracion de funciones flecha*/
-const divisioresV3 = numero =>{
-    let total = 0;
-    for(let i=1; i<=numero;i++){
-        if(numero % i == 0){
-            total++;
-        }
-    }
-    return total;
-};
-
-/*console.log(perfectos(4));*/
 
 function calcular(){
     let numero = parseInt(document.getElementById("num").value);
-
+    let numero2 = parseInt(document.getElementById("num2").value);
+    let operaciones = (document.getElementById("ope").value);
     let resultado = document.getElementById("resul");
 
-    resultado.value = divisores(numero);
-}
-/*Llamar la función, antes estaba esto aqui pero se sustituyo con resultado.value.
-console.log("divisiores",divisores(numero));*/
+    resultado.value = operaciones_calculo(numero, numero2, operaciones);
 
-/*Llamar la función*/
-/*console.log("divisioresV2",divisioresV2(33));
-console.log("divisioresV3",divisioresV2(33));*/
+}
+
+/*Segundo Ejercicio*/
+function imagenes(imag){
+    let imagen = 0;
+    if (imagenes == "imagen1") {
+        return imagen;
+    }
+    else if (imagenes == "imagen2"){
+        return imagen;
+    }
+    else if (imagenes == "imagen3"){
+        return imagen;
+    }
+    else if(imagenes == "imagen4"){
+        return imagen;
+    }
+    else if(imagenes == "imagen5"){
+        return imagen;
+    }
+}
+
+function verimagen(){
+    let imagen1 = (document.getElementById("imag").value);
+    let imagen2 = (document.getElementById("imag").value);
+    let imagen3 = (document.getElementById("imag").value);
+    let imagen4 = (document.getElementById("imag").value);
+    let imagen5 = (document.getElementById("imag").value);
+
+    resultado.value = imagenes();
+
+}
+
+/*5preguntas abieras y 3 programas javascript tipo, calculo, ciclos, if switch, variables, cajas normales*/
