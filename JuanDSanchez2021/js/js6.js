c257d0a0c026e535ce56bf65d772981ca2afca86
JuanDSanchez2021 <92406360+JuanDSanchez2021@users.noreply.github.com>
2022-10-29 14:45:56 -0500

    Commit

--- JuanDSanchez2021/js/js6.js
+++ JuanDSanchez2021/js/js6.js
@@ -1,4 +1,3 @@
-/* CALCULADORA */
 function funcionOperaciones(numero1, numero2, operador){
     let total = 0;
     if (operador == '+' || operador == 'suma') {
@@ -22,34 +21,31 @@ function funcionOperaciones(numero1, numero2, operador){
     }
 };
 
-// Funcion Ejercicio 2
-
-function procesar_img(){
-    let n_imagen = parseInt(document.getElementById("n_img").value);
-    let imagen = document.getElementById('img');
-    if (n_imagen == 1) {
-        imagen.src="https://image.api.playstation.com/vulcan/ap/rnd/202205/2800/E1Hw52cQH3HA6swkk1mcvbjc.png";
-        return imagen;
+function funcionImagen(){
+    let imgSeleccionada = parseInt(document.getElementById("n_img").value);
+    let img = document.getElementById('img');
+    if (imgSeleccionada == 1) {
+        img.src="https://image.api.playstation.com/vulcan/ap/rnd/202205/2800/E1Hw52cQH3HA6swkk1mcvbjc.png";
+        return img;
     }
-    else if (n_imagen == 2) {
-        imagen.src="https://images3.alphacoders.com/857/857335.jpg";
-        return imagen;
+    else if (imgSeleccionada == 2) {
+        img.src="https://images3.alphacoders.com/857/857335.jpg";
+        return img;
     }
-    else if (n_imagen == 3) {
-        imagen.src="https://s0.smartresize.com/wallpaper/391/556/HD-wallpaper-miles-morales-into-the-spider-verse-miles-morales-ps5-spider-man.jpg";
-        return imagen;
+    else if (imgSeleccionada == 3) {
+        img.src="https://s0.smartresize.com/wallpaper/391/556/HD-wallpaper-miles-morales-into-the-spider-verse-miles-morales-ps5-spider-man.jpg";
+        return img;
     }
-    else if (n_imagen == 4) {
-        imagen.src="https://p4.wallpaperbetter.com/wallpaper/935/86/550/cartoon-pictures-1920x1080-wallpaper-preview.jpg";
-        return imagen;
+    else if (imgSeleccionada == 4) {
+        img.src="https://p4.wallpaperbetter.com/wallpaper/935/86/550/cartoon-pictures-1920x1080-wallpaper-preview.jpg";
+        return img;
     }
-    else if (n_imagen == 5) {
-        imagen.src="https://images.alphacoders.com/986/986566.jpg";
-        return imagen;
+    else if (imgSeleccionada == 5) {
+        img.src="https://images.alphacoders.com/986/986566.jpg";
+        return img;
     }
     else {
-        Respuesta = 'Solo se acepta hasta el 5, vuelva a intentarlo';
-        return imagen;
+        return img;
     }
 };
 
